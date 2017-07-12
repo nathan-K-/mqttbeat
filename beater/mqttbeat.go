1c930828a9ec0d9f5fd3c61fa6f3430726573dec
nathan <nathan@innoflair.com>
2017-07-12 16:47:57 +0200

    first working version

--- beater/mqttbeat.go
+++ beater/mqttbeat.go
@@ -3,19 +3,32 @@ package beater
 import (
 	"fmt"
 	"time"
+	"encoding/json"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
 	"github.com/elastic/beats/libbeat/publisher"
 
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+
+	"gopkg.in/vmihailenco/msgpack.v2"
+
 	"github.com/nathan-k-/mqttbeat/config"
 )
 
 type Mqttbeat struct {
 	done   chan struct{}
-	config config.Config
-	client publisher.Client
+	beat_config config.Config
+	elastic_client publisher.Client
+	mqtt_client MQTT.Client
+}
+
+func setupMqttClient(bt *Mqttbeat) {
+	mqtt_client_opt := MQTT.NewClientOptions()
+	mqtt_client_opt.AddBroker(bt.beat_config.Broker_url)
+
+	bt.mqtt_client = MQTT.NewClient(mqtt_client_opt)
 }
 
 // Creates beater
@@ -27,36 +40,90 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 
 	bt := &Mqttbeat{
 		done:   make(chan struct{}),
-		config: config,
+		beat_config: config,
 	}
+	setupMqttClient(bt)
+
+	if token := bt.mqtt_client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	logp.Info("MQTT Client connected")
+
+	if token := bt.mqtt_client.SubscribeMultiple(bt.beat_config.Topics_subscribe, bt.on_message);
+	token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
 	return bt, nil
 }
 
+func decodePayload(payload []byte) common.MapStr {
+	event := make(common.MapStr)
+	sent := false
+
+	err := msgpack.Unmarshal(payload, &event)
+	if  err == nil {
+		err = json.Unmarshal(payload, &event)
+		if err == nil {
+			sent = true
+			logp.Info("Payload decoded - msgpack + json")
+			return event
+		} else {
+			sent = true
+			logp.Info("Payload decoded - msgpack")
+			return event
+		}
+	}
+
+	err = json.Unmarshal(payload, &event)
+	if  sent == false && err == nil {
+		sent = true
+		logp.Info("Payload decoded - json)")
+		return event
+	}
+
+	if sent == false {
+		sent = true
+		event["payload"]= string(payload)
+		logp.Info("Payload decoded - text")
+		//TODO handle case bytes ... ? (string fail)
+	}
+	return event
+}
+
+//define a function for the default message handler
+// see https://discuss.elastic.co/t/how-to-append-a-json-string-to-libbeat-event/34020/2
+func (bt *Mqttbeat) on_message(client MQTT.Client, msg MQTT.Message) {
+	logp.Info("MQTT MESSAGE RECEIVED " + string(msg.Payload()))
+	event := make(common.MapStr)
+	if bt.beat_config.Decode_paylod == true {
+		event = decodePayload(msg.Payload())
+	} else {
+		event = make(common.MapStr)
+		event["payload"] = msg.Payload()
+	}
+
+	event["beat"]= common.MapStr{"index": "mymqtt", "type":"message"}
+	event["@timestamp"] = common.Time(time.Now())
+	event["topic"] = msg.Topic()
+	bt.elastic_client.PublishEvent(event)
+	logp.Info("Event sent")
+}
+
+
 func (bt *Mqttbeat) Run(b *beat.Beat) error {
 	logp.Info("mqttbeat is running! Hit CTRL-C to stop it.")
+	bt.elastic_client = b.Publisher.Connect()
 
-	bt.client = b.Publisher.Connect()
-	ticker := time.NewTicker(bt.config.Period)
-	counter := 1
 	for {
 		select {
 		case <-bt.done:
 			return nil
-		case <-ticker.C:
 		}
-
-		event := common.MapStr{
-			"@timestamp": common.Time(time.Now()),
-			"type":       b.Name,
-			"counter":    counter,
-		}
-		bt.client.PublishEvent(event)
-		logp.Info("Event sent")
-		counter++
 	}
 }
 
 func (bt *Mqttbeat) Stop() {
-	bt.client.Close()
+	bt.elastic_client.Close()
 	close(bt.done)
 }
--- config/config.go
+++ config/config.go
@@ -7,8 +7,14 @@ import "time"
 
 type Config struct {
 	Period time.Duration `config:"period"`
+	Broker_url string `config:"broker_url"`
+	Topics_subscribe map[string]byte `config:"topics_subscribe"`
+	Decode_paylod bool `config:"decode_payload"`
 }
 
 var DefaultConfig = Config{
 	Period: 1 * time.Second,
+	Broker_url: "tcp://localhost:1883",
+	Topics_subscribe: map[string]byte{"/test/mqttbeat/#":1},
+	Decode_paylod: true,
 }
